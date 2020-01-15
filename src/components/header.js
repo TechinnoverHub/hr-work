51eadbc3bf6eea1f169c28c9aeff82336552a9af
JaybeeGeek <johnboscoobiejesi@gmail.com>
2020-01-15 11:50:26 +0100

    homepage corrections

--- src/components/header.js
+++ src/components/header.js
@@ -1,15 +1,15 @@
-import React, {Component} from "react";
-import {Link} from "react-router-dom";
-import logo from "./logo.svg";
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import logo from './logo.svg';
 
 export default class header extends Component {
-	render() {
-		return (
-			<div>
-				<Link to='/'>
-					<img src={logo} alt='logo' />
-				</Link>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <Link to="/">
+          <img src={logo} alt="logo" />
+        </Link>
+      </div>
+    );
+  }
 }
