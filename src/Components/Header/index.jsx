4682fac92652348459e98a86ab97872e291d2f23
Oluwatominiyin Adebanjo <tomiadebanjo@gmail.com>
2020-01-21 16:42:11 +0100

    implement sticky header functionality

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import { Link } from 'react-router-dom';
 import logo from 'Assets/svg/logo.svg';
+import logoDark from 'Assets/svg/DarkLogo.svg';
 import Styles from './index.module.scss';
 
 export default () => {
+  const [isSticky, setSticky] = useState(false);
+
+  const handleScroll = useCallback(() => {
+    setSticky(window.pageYOffset > 40);
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', () => handleScroll);
+    };
+  }, [handleScroll]);
+
   return (
-    <div className={Styles.header}>
+    <div
+      className={`${Styles.header} ${
+        isSticky ? Styles.sticky : Styles.stickOut
+      }`}
+    >
       <nav className={Styles.header__container}>
         <Link to="/">
-          <img src={logo} alt="logo" />
+          <img src={isSticky ? logoDark : logo} alt="logo" />
         </Link>
-        <ul className={Styles.link__wrapper}>
+        <ul
+          className={`${Styles.link__wrapper} ${
+            isSticky ? Styles.link_sticky : null
+          }`}
+        >
           <li>
             <Link className={Styles.link}>Who we are</Link>
           </li>
