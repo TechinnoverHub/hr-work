7698fa91e75a5ae135d21019dd3a2dc28d992f73
Oluwatominiyin Adebanjo <tomiadebanjo@gmail.com>
2020-01-26 01:48:46 +0100

    implement contact page corrections

--- src/App.js
+++ src/App.js
@@ -2,13 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Home from 'Pages/Home';
-<<<<<<< HEAD
 import About from 'Pages/NewAbout';
 import Career from 'Pages/Career';
-=======
-import About from 'Pages/NewAbout/about';
-import Contact from 'Pages/Contact/contact';
->>>>>>> contact-page
+import Contact from 'Pages/Contact';
 
 export default function App() {
   return (
@@ -20,13 +16,11 @@ export default function App() {
         <Route path="/about">
           <About />
         </Route>
-<<<<<<< HEAD
         <Route path="/career">
           <Career />
-=======
-        <Route path="/contact" component={Contact}>
+        </Route>
+        <Route path="/contact">
           <Contact />
->>>>>>> contact-page
         </Route>
       </Switch>
     </Router>
