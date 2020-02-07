ba67a1b059e97227ba0501081118d856b5ebe560
Oluwatominiyin Adebanjo <tomiadebanjo@gmail.com>
2020-02-07 16:43:52 +0100

    review account details page

--- src/Pages/AccountDetails/index.jsx
+++ src/Pages/AccountDetails/index.jsx
@@ -1,108 +1,59 @@
 import React from 'react';
 import './index.scss';
-import Header from 'Components/Header';
-import Footer from 'Components/Footer';
-import { Link } from 'react-router-dom';
 
-class AccountDetails extends React.Component {
-  render() {
-    return (
-      <di>
-        <Header />
-        <div className="account-wrapper">
-          <div className="account-section">
-            <div className="account-left-box-wrapper">
-              <div className="account-left-box-text">My account</div>
-              <div className="account-left-box">
-                <ul>
-                  <li>
-                    <Link className="account-left-list">Dashboard</Link>
-                  </li>
-                  <div className="account-left-horizontal-line"></div>
-                  <li>
-                    <Link className="account-left-list">Orders</Link>
-                  </li>
-                  <div className="account-left-horizontal-line"></div>
+const AccountDetails = () => {
+  return (
+    <div className="account-right-box-wrapper">
+      <div className="account-form">
+        <form className="account-form-container">
+          <div className="name-row">
+            <label>
+              First Name
+              <input type="text" placeholder="Enter First Name" />
+            </label>
 
-                  <li>
-                    <Link className="account-left-list">accountes</Link>
-                  </li>
-                  <div className="account-left-horizontal-line"></div>
-
-                  <li>
-                    <Link className="account-left-list">Payment Methods</Link>
-                  </li>
-                  <div className="account-left-horizontal-line"></div>
-
-                  <li>
-                    <Link className="account-left-list">account Details</Link>
-                  </li>
-                  <div className="account-left-horizontal-line"></div>
-
-                  <li>
-                    <Link className="account-left-list">Logout</Link>
-                  </li>
-                  <div className="account-left-horizontal-line"></div>
-                </ul>
-              </div>
-            </div>
-
-            <div className="account-right-box-wrapper">
-              <div className="account-form">
-                <form className="account-form-container">
-                  <div className="name-row">
-                    <label>
-                      First Name
-                      <input type="text" placeholder="Enter First Name" />
-                    </label>
-
-                    <label>
-                      Last Name
-                      <input type="text" placeholder="Enter Last Name" />
-                    </label>
-                  </div>
-
-                  <div className="field-row">
-                    <label>
-                      Email account
-                      <input type="email" placeholder="Enter Email account" />
-                    </label>
-                  </div>
-                  <div className="change-password-text">Password change</div>
-                  <div className="change-password-section"></div>
-                  <div className="field-row">
-                    <label>
-                      Current password
-                      <span>(leave blank to leave unchanged)</span>
-                      <input type="password" />
-                    </label>
-                  </div>
-
-                  <div className="field-row">
-                    <label>
-                      New password<span>(leave blank to leave unchanged)</span>
-                      <input type="password" />
-                    </label>
-                  </div>
-
-                  <div className="field-row">
-                    <label>
-                      Confirm new password
-                      <input type="password" />
-                    </label>
-                  </div>
-                  <div className="account-save-button">
-                    <button type="submit">Save changes</button>
-                  </div>
-                </form>
-              </div>
-            </div>
+            <label>
+              Last Name
+              <input type="text" placeholder="Enter Last Name" />
+            </label>
           </div>
-        </div>
-        <Footer />
-      </di>
-    );
-  }
-}
+
+          <div className="account-field-row">
+            <label>
+              Email account
+              <input type="email" placeholder="Enter Email account" />
+            </label>
+          </div>
+          <div className="change-password-text">Password change</div>
+          <div className="change-password-section"></div>
+          <div className="account-field-row">
+            <label>
+              Current password
+              <span>(leave blank to leave unchanged)</span>
+              <input type="password" />
+            </label>
+          </div>
+
+          <div className="account-field-row">
+            <label>
+              New password<span>(leave blank to leave unchanged)</span>
+              <input type="password" />
+            </label>
+          </div>
+
+          <div className="account-field-row">
+            <label>
+              Confirm new password
+              <input type="password" />
+            </label>
+          </div>
+          <div className="account-save-button">
+            <button type="submit">Save changes</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default AccountDetails;
